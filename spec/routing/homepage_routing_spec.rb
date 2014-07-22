09fda63827d82f86688ac4fccd807dd2820b6df8
Jake Moffatt <jakeonrails@gmail.com>
2014-07-21 19:27:37 -0700

    Fix Post routing specs and add Homepage routing spec

---
+++ spec/routing/homepage_routing_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe HomepageController, type: :routing do
+  describe "routing" do
+
+    it "routes to #index" do
+      expect(get: "/").to route_to("homepage#index")
+    end
+
+  end
+end
--- spec/routing/posts_routing_spec.rb
+++ spec/routing/posts_routing_spec.rb
@@ -1,34 +1,34 @@
 require "rails_helper"
 
-RSpec.describe PostsController, :type => :routing do
+RSpec.describe PostsController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/posts").to route_to("posts#index")
+      expect(get: "/username").to route_to("posts#index", user_id: 'username')
     end
 
     it "routes to #new" do
-      expect(:get => "/posts/new").to route_to("posts#new")
+      expect(get: "/username/new").to route_to("posts#new", user_id: 'username')
     end
 
     it "routes to #show" do
-      expect(:get => "/posts/1").to route_to("posts#show", :id => "1")
+      expect(get: "/username/1").to route_to("posts#show", id: "1", user_id: 'username')
     end
 
     it "routes to #edit" do
-      expect(:get => "/posts/1/edit").to route_to("posts#edit", :id => "1")
+      expect(get: "/username/1/edit").to route_to("posts#edit", id: "1", user_id: 'username')
     end
 
     it "routes to #create" do
-      expect(:post => "/posts").to route_to("posts#create")
+      expect(post: "/username").to route_to("posts#create", user_id: 'username')
     end
 
     it "routes to #update" do
-      expect(:put => "/posts/1").to route_to("posts#update", :id => "1")
+      expect(put: "/username/1").to route_to("posts#update", id: "1", user_id: 'username')
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/posts/1").to route_to("posts#destroy", :id => "1")
+      expect(delete: "/username/1").to route_to("posts#destroy", id: "1", user_id: 'username')
     end
 
   end
